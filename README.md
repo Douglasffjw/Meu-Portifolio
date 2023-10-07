6637b5cd58839d8943fb9fc7742b8710419a6693
Douglasffjw <douglasffjw@gmail.com>
2023-10-06 22:37:14 -0300

    ok

---
+++ README.md
@@ -0,0 +1,45 @@
+<h1 align="center">
+  💻<br>Portfólio - Douglas Fernandes 
+</h1>
+
+![Resultado final do projeto](assets/image/preview.png)
+
+<h4 align="center"><a href="https://meu-portifolio-dg.vercel.app/" target="_blank">Clique para visitar o projeto</a></h4>
+
+## 📚 Seções
+
+O site é composto por cinco seções:
+
+- **Start:** Nele temos uma breve apresentação;
+- **Sobre mim:** Nessa seção tenho uma descrição dizendo um pouco sobre quem sou;
+- **Projetos:** Apresenta alguns projetos desenvolvidos e com link direto para os respectivos códigos no GitHub;
+- **Conhecimentos:** Nele apresentamos meus conhecimentos em algumas linguagens como o foco no front-end;
+
+---
+
+## 💼 Tecnologias utilizadas
+
+Para o desenvolvimento deste site utilizei as seguintes tecnologias:
+
+- HTML;
+- Sass;
+- JavaScript;
+- ScrollReveal;
+- SVG Inject;
+
+---
+
+<h2><img src="https://media.giphy.com/media/12oufCB0MyZ1Go/giphy.gif" width="50"> Autor</h2>
+
+<table>
+  <tr>
+    <td align="center">
+      <a href="https://github.com/Douglasffjw">
+        <img src="https://github.com/Douglasffjw.png" width="100px;" alt="Foto do Dg no GitHub"/><br>
+        <sub>
+          <b>Douglas Fernandes</b>
+        </sub>
+      </a>
+    </td>
+  </tr>
+</table>
---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+import { initScrollReveal } from "./scrollReveal.js";
+import { hoverChangeExperience } from "./hoverChangeExperience.js";
+import { typeWrite } from "./typeWrite.js";
+import { hoverChangeDescription } from "./hoverChangeDescription.js";
+import { menu } from "./menu.js";
+
+menu();
+initScrollReveal();
+typeWrite(document.querySelector(".typewriter"));
+
+hoverChangeDescription(
+  ".html",
+  "HTML é uma linguagem de marcação, onde marcamos os elementos para definir quais informações a página vai exibir."
+);
+hoverChangeDescription(
+  ".css",
+  "CSS é uma linguagem de folha de estilo composta por “camadas”, criado com o propósito de estilizar as páginas."
+);
+hoverChangeDescription(
+  ".js",
+  "JavaScript é uma linguagem de programação que permite a você implementar elementos dinâmicos em páginas web."
+);
+hoverChangeDescription(
+  ".py",
+  "Python é uma linguagem de programação de alto nível, interpretada de script, imperativa, orientada a objetos, funcional, de tipagem dinâmica e forte."
+);
+hoverChangeDescription(
+  ".git",
+  "Git é o sistema de controle de versão open source mais usado no mundo atualmente! Ele é usado para controlar o histórico de alterações de arquivos e principalmente de projetos de desenvolvimento de software. Ele permite mais flexibilidade no fluxo de trabalho, segurança e desempenho."
+);
